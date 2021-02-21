f43f7e498a1d3a1dc876c3b0988e5d6115831551
George Dilthey <george.dilthey@gmail.com>
2021-02-21 13:14:34 -0500

    commit tree f1bc9a2af78b728c09fc61cd85e19bbaa0be8c9b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each {|obj| puts obj.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear()
+    end
+
+end
+
+# fido = Dog.new('fido')
+# Dog.print_all
